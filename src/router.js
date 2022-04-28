2c7f6b88044ec849113f3a49fc0c12c886c75fc8
ivibarros <ivibarros19@gmail.com>
2022-04-27 22:38:07 -0700

    2

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 // imports of pages
-import HelloWorld from './components/HelloWorld.vue'
-import Page2 from './components/page2.vue'
+import HelloWorld from './pages/HelloWorld.vue'
+import Page2 from './pages/page2.vue'
 
 // routes
 const routes = [
